1cd2bc324cd0024e9246d46019f8eaa9bea73e4a
Nydoehar <95442777+Nydoehar@users.noreply.github.com>
2023-04-29 12:39:04 -0300

    Update install_dots.sh

--- install_dots.sh
+++ install_dots.sh
@@ -3,11 +3,12 @@
 # Make script executable < chmod +x Install_dots.sh
 
 (
-cp ~/Dotfiles/.bashrc ~/Dotfiles/.bash_aliases ~/
+cp ~/dotfiles/.bashrc ~/dotfiles/.bash_aliases ~/
 rm -rf ~/Dotfiles
 exec bash
-c
 echo "Done!"
+sleep 2
+c
 )
 
 
