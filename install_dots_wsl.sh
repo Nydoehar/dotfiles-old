e258e9b3d2ac3f2fd5c991e1e1a000170919b66e
Nydoehar <95442777+Nydoehar@users.noreply.github.com>
2023-05-20 14:28:21 -0300

    Update install_dots_wsl.sh
    Script to copy dot files to the system.

--- install_dots_wsl.sh
+++ install_dots_wsl.sh
@@ -5,6 +5,8 @@
 (
 cp ~/dotfiles/.bashrc ~/dotfiles/.bash_aliases-wsl ~/
 mv ~/.bash_aliases-wsl .bash_aliases
+mkdir -p ~/.config/nvim/
+cp ~/dotfiles/init.lua ~/.config/nvim/
 rm -rf ~/dotfiles
 exec bash
 c
