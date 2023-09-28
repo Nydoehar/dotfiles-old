7f4cebe713518dc7dc37ada853e837e4093a58ae
Nydoehar <95442777+Nydoehar@users.noreply.github.com>
2023-09-28 20:20:07 -0300

    post_installation.sh
    Remove apt update and git install since I did that in the script "exec_post_install" to execute this one.

--- post_installation.sh
+++ post_installation.sh
@@ -2,10 +2,9 @@
 
 # Script to setup a new Debian 12 installation.
 
-sudo apt update \
 sudo apt -y upgrade \
 sudo apt -y purge thunderbird firefox \
-sudo apt -y install gufw git neovim timeshift brave-browser \
+sudo apt -y install gufw neovim timeshift brave-browser \
 sudo apt -y autoremove
 sudo ufw enable \
 sudo ufw status verbose
