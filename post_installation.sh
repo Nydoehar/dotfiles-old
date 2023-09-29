e6141c0e823c1d27af4a43edf82c8d03c6437070
Nydoehar <95442777+Nydoehar@users.noreply.github.com>
2023-09-29 20:32:58 -0300

    post_installation.sh

--- post_installation.sh
+++ post_installation.sh
@@ -2,9 +2,9 @@
 
 # Script to setup a new Debian 12 installation.
 
-#sudo apt upgrade -y \
-sudo apt purge -y thunderbird firefox \
-sudo apt install -y gufw neovim timeshift brave-browser \
-sudo apt autoremove -y \
+sudo apt upgrade -y && \
+sudo apt purge -y thunderbird firefox && \
+sudo apt install -y gufw neovim timeshift brave-browser && \
+sudo apt autoremove -y && \
 #sudo ufw enable \
 sudo ufw status verbose \
