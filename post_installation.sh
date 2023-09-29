866fa60e71e56380505734617beb6b1e53c3ac9e
Nydoehar <95442777+Nydoehar@users.noreply.github.com>
2023-09-29 20:33:41 -0300

    post_installation.sh

--- post_installation.sh
+++ post_installation.sh
@@ -6,5 +6,5 @@ sudo apt upgrade -y && \
 sudo apt purge -y thunderbird firefox && \
 sudo apt install -y gufw neovim timeshift brave-browser && \
 sudo apt autoremove -y && \
-#sudo ufw enable \
+sudo ufw enable && \
 sudo ufw status verbose \
