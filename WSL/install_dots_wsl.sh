29bbb70d0ee161889465c1f64a31d69542fe43bc
Nydoehar <95442777+Nydoehar@users.noreply.github.com>
2023-10-02 20:06:31 -0300

    Update install_dots_wsl.sh

--- WSL/install_dots_wsl.sh
+++ WSL/install_dots_wsl.sh
@@ -3,14 +3,11 @@
 # Make script executable < chmod +x Install_dots.sh
 
 (
-cp ~/dotfiles/.bashrc ~/dotfiles/.bash_aliases-wsl ~/
+cp ~/dotfiles/WSL/.bashrc ~/dotfiles/WSL/.bash_aliases-wsl ~/
 mv ~/.bash_aliases-wsl .bash_aliases
 mkdir -p ~/.config/nvim/
-cp ~/dotfiles/init.lua ~/.config/nvim/
+cp ~/dotfiles/Nvim/init.lua ~/.config/nvim/
 rm -rf ~/dotfiles
 exec bash
 c
 )
-
-
-
