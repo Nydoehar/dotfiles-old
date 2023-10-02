270821ab93e83b211d4d9e43d2560db84525cc7d
Nydoehar <95442777+Nydoehar@users.noreply.github.com>
2023-10-02 19:53:39 -0300

    Update and rename install_dots.sh to exec_post_install.sh

--- Debian/exec_post_install.sh
+++ Debian/exec_post_install.sh
@@ -6,8 +6,9 @@
 cp ~/dotfiles/Debian/.bashrc ~/dotfiles/Debian/.bash_aliases ~/
 mkdir -p ~/.config/nvim/
 cp ~/dotfiles/Nvim/init.lua ~/.config/nvim/
+bash ~/dotfiles/Debian/post_install_script.sh
 rm -rf ~/dotfiles
 exec bash
-c
+#c
 )
 
