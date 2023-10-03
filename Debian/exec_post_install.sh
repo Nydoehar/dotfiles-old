7f60a2f495e4c80b880301b9146a7329c7673e4a
Nydoehar <95442777+Nydoehar@users.noreply.github.com>
2023-10-03 19:51:33 -0300

    Update exec_post_install.sh

--- Debian/exec_post_install.sh
+++ Debian/exec_post_install.sh
@@ -3,9 +3,9 @@
 # Make script executable < chmod +x install_dots.sh
 
 (
-cp ~/dotfiles/Debian/.bashrc ~/dotfiles/Debian/.bash_aliases ~/
+cp ~/dotfiles/General/.bashrc ~/dotfiles/Debian/.bash_aliases ~/
 mkdir -p ~/.config/nvim/
-cp ~/dotfiles/Nvim/init.lua ~/.config/nvim/
+cp ~/dotfiles/General/Neovim/init.lua ~/.config/nvim/
 bash ~/dotfiles/Debian/post_install_script.sh
 rm -rf ~/dotfiles
 exec bash
