9591e832488b45115d2fa0a4ed20b7afb40bf9f0
Nydoehar <95442777+Nydoehar@users.noreply.github.com>
2023-10-04 20:05:50 -0300

    Update fedora-post_install_script.sh

--- fedora-post_install_script.sh
+++ fedora-post_install_script.sh
@@ -18,19 +18,19 @@ exec bash
 timedatectl set-ntp true 
 timedatectl set-local-rtc 1 --adjust-system-clock
 
-sudo dnf upgrade -y && \
-sudo dnf remove -y thunderbird firefox && \
-sudo dnf install -y neovim timeshift && \
+sudo dnf upgrade -y
+sudo dnf remove -y thunderbird firefox
+sudo dnf install -y neovim timeshift
 
 # Brave Browser start
-sudo dnf install dnf-plugins-core && \
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo && \
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc && \
-sudo dnf install brave-browser brave-keyring && \
+sudo dnf install dnf-plugins-core
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo dnf install brave-browser brave-keyring
 # Brave Browser end
 
-sudo dnf autoremove -y && \
-systemctl status firewalld && \
+sudo dnf autoremove -y
+systemctl status firewalld
 
 # Mount partitions start
 sudo mkdir /mnt/HDD-1 /mnt/NVME-1
@@ -43,3 +43,4 @@ cp -r /mnt/HDD-1/MY_STUFF/PC/_Dot_Files/transmission ~/.config/
 echo " " >> /etc/fstab
 echo "UUID=2B2F6B4529BC9532 /mnt/HDD-1 ntfs uid=1000, gid =1000, rw,user, auto, exec 0 0" >> /etc/fstab
 echo "UUID=74A80C647FDD4806 /mnt/NVME-1 ntfs uid=1000, gid =1000, rw,user, auto, exec 0 0" >> /etc/fstab
+
