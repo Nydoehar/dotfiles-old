9969684004d58900b86c8e24b914438546682f49
Nydoehar <95442777+Nydoehar@users.noreply.github.com>
2023-10-04 21:27:01 -0300

    Update fedora-post_install_script.sh

--- fedora-post_install_script.sh
+++ fedora-post_install_script.sh
@@ -2,7 +2,6 @@
 
 # Script to setup a new Fedora installation.
 
-(
 cp ~/dotfiles/.bashrc ~/dotfiles/fedora-bash_aliases ~/
 mv ~/fedora-bash_aliases ~/.bash_aliases
 mkdir -p ~/.config/nvim/
@@ -12,22 +11,19 @@ mkdir -p ~/.local/bin
 cp ~/dotfiles/Scripts/Wrappers/* ~/.local/bin/
 (cd ~/dotfiles/ && dconf load / < Gnome_Settings-Full_Backup)
 rm -rf ~/dotfiles
-exec bash
-#c
-)
 
 timedatectl set-ntp true 
 timedatectl set-local-rtc 1 --adjust-system-clock
 
-yes | sudo dnf upgrade 
-sudo dnf remove -y thunderbird firefox && \
-sudo dnf install -y neovim timeshift && \
+sudo dnf upgrade -y
+sudo dnf remove -y thunderbird firefox
+sudo dnf install -y neovim timeshift
 
 # Brave Browser start
 sudo dnf install dnf-plugins-core
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo dnf install brave-browser brave-keyring
+sudo dnf install -y brave-browser brave-keyring
 # Brave Browser end
 
 sudo dnf autoremove -y
@@ -44,4 +40,3 @@ cp -r /mnt/HDD-1/MY_STUFF/PC/_Dot_Files/transmission ~/.config/
 echo " " >> /etc/fstab
 echo "UUID=2B2F6B4529BC9532 /mnt/HDD-1 ntfs uid=1000, gid =1000, rw,user, auto, exec 0 0" >> /etc/fstab
 echo "UUID=74A80C647FDD4806 /mnt/NVME-1 ntfs uid=1000, gid =1000, rw,user, auto, exec 0 0" >> /etc/fstab
-
