a57153cb47e9367dd1c4b6db9409e397ac3a3aab
Nydoehar <95442777+Nydoehar@users.noreply.github.com>
2023-10-12 21:07:04 -0300

    Create post_install_script.sh

---
+++ Debian/post_install_script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Script to setup a new Debian 12 installation.
+
+cp ~/dotfiles/.bashrc ~/dotfiles/debian-bash_aliases ~/
+mv ~/debian-bash_aliases ~/.bash_aliases
+mkdir -p ~/.config/nvim/
+cp ~/dotfiles/neovim-init.lua ~/.config/nvim/
+mv ~/.config/nvim/neovim-init.lua ~/.config/nvim/init.lua
+mkdir -p ~/.local/bin
+cp ~/dotfiles/Scripts/Wrappers/* ~/.local/bin/
+(cd ~/.local/bin/ && chmod +x *)
+mv ~/dotfiles/debian-sources.list ~/dotfiles/sources.list
+sudo cp ~/dotfiles/sources.list /etc/apt/
+(cd ~/dotfiles/ && dconf load / < Gnome_Settings-Full_Backup)
+rm -rf ~/dotfiles
+
+timedatectl set-ntp true 
+timedatectl set-local-rtc 1 --adjust-system-clock
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt purge -y thunderbird
+sudo apt install -y gufw nvidia-driver firmware-misc-nonfree vlc neovim timeshift node-typescript make
+
+git clone https://github.com/pop-os/shell.git
+(cd shell && yes | make local-install)
+
+sudo apt autoremove -y
+sudo ufw enable
+sudo ufw status verbose
+
+sudo mkdir /mnt/HDD-1 /mnt/NVME-1
+sudo mount -t ntfs UUID=2B2F6B4529BC9532 /mnt/HDD-1
+sudo mount -t ntfs UUID=74A80C647FDD4806 /mnt/NVME-1
+
+cp -r /mnt/HDD-1/MY_STUFF/PC/_Dot_Files/transmission ~/.config/
+
+sudo su -c "echo ' ' >> /etc/fstab"
+sudo su -c "echo 'UUID=2B2F6B4529BC9532 /mnt/HDD-1 ntfs uid=1000, gid =1000, rw,user, auto, exec 0 0' >> /etc/fstab"
+sudo su -c "echo 'UUID=74A80C647FDD4806 /mnt/NVME-1 ntfs uid=1000, gid =1000, rw,user, auto, exec 0 0' >> /etc/fstab"
