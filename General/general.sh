f8f5fa174c2f0d85a6e6edca5137b08c3c9af837
Nydoehar <95442777+Nydoehar@users.noreply.github.com>
2023-11-21 09:59:22 -0300

    Create general.sh

---
+++ General/general.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp ~/dotfiles/General/.bashrc ~/dotfiles/Debian/.bash_aliases ~/
+mkdir -p ~/.config/nvim/
+cp ~/dotfiles/General/neovim-init.lua ~/.config/nvim/
+mv ~/.config/nvim/neovim-init.lua ~/.config/nvim/init.lua
+mkdir -p ~/.local/bin
+cp ~/dotfiles/Scripts/Wrappers/* ~/.local/bin/
+(cd ~/.local/bin/ && chmod +x *)
+rm -rf ~/dotfiles
