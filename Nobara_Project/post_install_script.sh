d11227eaab300875ecf815fee395551daed21d64
Nydoehar <95442777+Nydoehar@users.noreply.github.com>
2024-01-03 18:00:40 -0300

    Update post_install_script.sh

--- Nobara_Project/post_install_script.sh
+++ Nobara_Project/post_install_script.sh
@@ -9,7 +9,7 @@ mv ~/.config/nvim/neovim-init.lua ~/.config/nvim/init.lua
 mkdir -p ~/.local/bin
 cp ~/dotfiles/Scripts/Wrappers/* ~/.local/bin/
 (cd ~/.local/bin/ && chmod +x *)
-(cd ~/dotfiles/General/ && dconf load / < Gnome_Settings-Full_Backup)
+#(cd ~/dotfiles/General/ && dconf load / < Gnome_Settings-Full_Backup)
 mkdir -p ~/.config/autostart
 mv ~/dotfiles/.desktop_files/transmission-gtk.desktop ~/dotfiles/.desktop_files/com.brave.Browser.desktop ~/.config/autostart/
 rm -rf ~/dotfiles
@@ -18,24 +18,25 @@ timedatectl set-ntp true
 timedatectl set-local-rtc 1 --adjust-system-clock
 
 sudo dnf upgrade -y
-sudo dnf remove -y thunderbird firefox
-sudo dnf install -y transmission vlc neovim timeshift typescript.noarch make gnome-shell-extension-pop-shell xprop
+sudo dnf remove -y thunderbird
+sudo dnf install -y transmission vlc neovim timeshift 
+#sudo dnf install -y transmission vlc neovim timeshift typescript.noarch make gnome-shell-extension-pop-shell xprop
 flatpak install flathub --user -y com.brave.Browser
 flatpak uninstall --unused
 
 sudo dnf autoremove -y
 q | systemctl status firewalld
 
-#sudo mkdir /mnt/HDD-1 /mnt/NVME-1
-#sudo mount -t ntfs UUID=2B2F6B4529BC9532 /mnt/HDD-1
-#sudo mount -t ntfs UUID=74A80C647FDD4806 /mnt/NVME-1
+sudo mkdir /mnt/HDD-1 /mnt/NVME-1
+sudo mount -t ntfs UUID=2B2F6B4529BC9532 /mnt/HDD-1
+sudo mount -t ntfs UUID=74A80C647FDD4806 /mnt/NVME-1
 
-#cat /mnt/HDD-1/MY_STUFF/PC/Dot_Files/fstab_lines | sudo tee -a /etc/fstab
+cat /mnt/HDD-1/MY_STUFF/PC/Dot_Files/fstab_lines | sudo tee -a /etc/fstab
 
-#flatpak run com.brave.Browser
-#transmission-gtk
+flatpak run com.brave.Browser
+transmission-gtk
 
-#cp -r /mnt/HDD-1/MY_STUFF/PC/Dot_Files/transmission ~/.config/
-#cp -r /mnt/HDD-1/MY_STUFF/PC/Dot_Files/Brave-Browser ~/.var/app/com.brave.Browser/config/BraveSoftware/
+cp -r /mnt/HDD-1/MY_STUFF/PC/Dot_Files/transmission ~/.config/
+cp -r /mnt/HDD-1/MY_STUFF/PC/Dot_Files/Brave-Browser ~/.var/app/com.brave.Browser/config/BraveSoftware/
 
 exec bash
