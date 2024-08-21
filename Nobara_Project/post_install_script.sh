7b8cc399a4c0bde09d57b77065bf9881e4a12199
Nydoehar <95442777+Nydoehar@users.noreply.github.com>
2024-08-21 14:40:59 -0300

    Update post_install_script.sh

--- Nobara_Project/post_install_script.sh
+++ Nobara_Project/post_install_script.sh
@@ -11,7 +11,7 @@ cp ~/dotfiles/Scripts/Wrappers/* ~/.local/bin/
 (cd ~/.local/bin/ && chmod +x *)
 #(cd ~/dotfiles/General/ && dconf load / < Gnome_Settings-Full_Backup)
 mkdir -p ~/.config/autostart
-mv ~/dotfiles/.desktop_files/transmission-gtk.desktop ~/dotfiles/.desktop_files/com.brave.Browser.desktop ~/.config/autostart/
+#mv ~/dotfiles/.desktop_files/transmission-gtk.desktop ~/dotfiles/.desktop_files/com.brave.Browser.desktop ~/.config/autostart/
 rm -rf ~/dotfiles
 
 timedatectl set-ntp true 
@@ -19,24 +19,21 @@ timedatectl set-local-rtc 1 --adjust-system-clock
 
 sudo dnf upgrade -y
 sudo dnf remove -y thunderbird
-sudo dnf install -y transmission vlc neovim timeshift 
+#sudo dnf install -y vlc neovim timeshift 
 #sudo dnf install -y transmission vlc neovim timeshift typescript.noarch make gnome-shell-extension-pop-shell xprop
-flatpak install flathub --user -y com.brave.Browser
-flatpak uninstall --unused
+#flatpak install flathub --user -y com.brave.Browser
+#flatpak uninstall --unused
 
 sudo dnf autoremove -y
 q | systemctl status firewalld
 
-sudo mkdir /mnt/HDD-1 /mnt/NVME-1
+sudo mkdir /mnt/HDD /mnt/NVME
 sudo mount -t ntfs UUID=2B2F6B4529BC9532 /mnt/HDD-1
-sudo mount -t ntfs UUID=74A80C647FDD4806 /mnt/NVME-1
+sudo mount -t ntfs UUID=6B4FA4C31DAB7943 /mnt/NVME-1
 
-cat /mnt/HDD-1/MY_STUFF/PC/Dot_Files/fstab_lines | sudo tee -a /etc/fstab
+cat /mnt/HDD/MY_STUFF/PC/Dot_Files/fstab_lines | sudo tee -a /etc/fstab
+cat /mnt/HDD/MY_STUFF/PC/Dot_Files/root_aliases | sudo tee -a /root/.bashrc
 
-flatpak run com.brave.Browser
-transmission-gtk
-
-cp -r /mnt/HDD-1/MY_STUFF/PC/Dot_Files/transmission ~/.config/
 cp -r /mnt/HDD-1/MY_STUFF/PC/Dot_Files/Brave-Browser ~/.var/app/com.brave.Browser/config/BraveSoftware/
 
 exec bash
