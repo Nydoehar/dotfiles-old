ffe1f044ff0b40c06d9e911eb980b107fd214eed
Nydoehar <95442777+Nydoehar@users.noreply.github.com>
2024-08-21 20:04:13 -0300

    Update post_install_script.sh

--- Nobara_Project/post_install_script.sh
+++ Nobara_Project/post_install_script.sh
@@ -2,6 +2,10 @@
 
 # Script to setup a new Nobara Project installation.
 
+sudo dnf upgrade -y
+sudo dnf install podman distrobox neovim
+sudo dnf autoremove -y
+
 cp ~/dotfiles/General/.bashrc ~/dotfiles/Nobara_Project/.bash_aliases ~/
 mkdir -p ~/.config/nvim/
 cp ~/dotfiles/General/neovim-init.lua ~/.config/nvim/
@@ -17,9 +21,6 @@ rm -rf ~/dotfiles
 timedatectl set-ntp true 
 timedatectl set-local-rtc 1 --adjust-system-clock
 
-sudo dnf upgrade -y
-sudo dnf install podman distrobox
-sudo dnf autoremove -y
 q | systemctl status firewalld
 
 sudo mkdir /mnt/HDD /mnt/NVME
@@ -32,5 +33,6 @@ cat /mnt/HDD/MY_STUFF/PC/Dot_Files/root_aliases | sudo tee -a /root/.bashrc
 cp -r /mnt/HDD-1/MY_STUFF/PC/Dot_Files/Brave-Browser ~/.var/app/com.brave.Browser/config/BraveSoftware/
 
 distrobox create --image quay.io/toolbx/arch-toolbox:latest arch_linux_box
+distrobox enter --name arch-linux-box
 
 exec bash
