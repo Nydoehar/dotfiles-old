e7e55ccfb05e4f29daf613fdc7524d4456fbe82d
Nydoehar <95442777+Nydoehar@users.noreply.github.com>
2026-01-01 08:57:05 -0300

    Update post_install_script.sh
    Update paths.

--- Proxmox/post_install_script.sh
+++ Proxmox/post_install_script.sh
@@ -1,8 +1,8 @@
 mkdir -p ~/.config/nvim/
-cp ~/dotfiles/General/neovim-init.lua ~/.config/nvim/
+cp ~/dotfiles-old/General/neovim-init.lua ~/.config/nvim/
 mv ~/.config/nvim/neovim-init.lua ~/.config/nvim/init.lua
-cat ~/dotfiles/Proxmox/aliases | tee -a ~/.bashrc
-rm -rf ~/dotfiles
+cat ~/dotfiles-old/Proxmox/aliases | tee -a ~/.bashrc
+rm -rf ~/dotfiles-old
 
 apt install neovim 
 
